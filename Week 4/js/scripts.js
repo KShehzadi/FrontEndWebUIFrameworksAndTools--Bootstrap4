2a14b398abf70969f96d082e3e7358aa2c943be9
Komal303 <shehzadikomal303@gmail.com>
2020-08-13 19:27:50 +0500

    NPM Scripts Part 1

---
+++ Week 4/js/scripts.js
@@ -0,0 +1,26 @@
+
+        jQuery(document).ready(function(){
+            jQuery("#mycarousel").carousel( { interval: 2000 } );
+            jQuery("#carouselButton").click(function()
+            {
+                
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $('#mycarousel').carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $('#mycarousel').carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');
+                }
+            });
+            $("#loginButton").click(function(){
+                    $('#loginModal').modal('show');
+            });
+
+            $("#reserveTableButton").click(function(){
+                    $('#reserveModal').modal('show');
+            });
+           
+        });
